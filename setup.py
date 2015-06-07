bc8f7ac4d9796759fb2a2567509d7f7cad1b1715
Alexander Grothe <seahawk1986@gmx.de>
2015-06-07 19:54:07 +0200

    added setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+setup(name="hello_world",
+      version='0.0.1',
+      py_modules=['hello_world']
+      entry_points = {
+              'console_scripts': [
+                  'hello_world = hello_word:print_hello_world',                  
+              ],              
+          },
+)
